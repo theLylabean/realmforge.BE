ade494549b7391ee78410f7711a6b98840c75200
Lyla Lynn <lyladlynn@gmail.com>
2025-09-09 20:03:53 -0500

    adding data

--- db/schema/armor.sql
+++ db/schema/armor.sql
@@ -1,7 +1,7 @@
 CREATE TABLE armor (
     id SERIAL PRIMARY KEY,
-    equipment_id INTEGER REFERENCES equipment_list(id) ON DELETE CASCADE,
-    armor_category TEXT NOT NULL,          -- e.g., "Light", "Medium", "Heavy"
+    equipment_id INTEGER REFERENCES equipment_list(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    armor_category TEXT NOT NULL, 
     base_ac INTEGER NOT NULL,
     dex_bonus_allowed BOOLEAN,
     stealth_disadvantage BOOLEAN
