289db9d33eddfacb852251304b5c8c2b9efd2bf6
Lyla Lynn <lyladlynn@gmail.com>
2026-06-19 10:06:29 -0500

    updating schema and optimizing workflow to match 5e character sheet.

--- db/schema/enums.sql
+++ db/schema/enums.sql
@@ -5,15 +5,15 @@ DO $$ BEGIN
   CREATE TYPE ability AS ENUM ('STR','DEX','CON','INT','WIS','CHA');
 EXCEPTION WHEN duplicate_object THEN NULL; END $$;
 
--- Movement modes
-DO $$ BEGIN
-  CREATE TYPE speed_mode AS ENUM ('walk','swim','fly','climb','burrow');
-EXCEPTION WHEN duplicate_object THEN NULL; END $$;
+-- -- Movement modes
+-- DO $$ BEGIN
+--   CREATE TYPE speed_mode AS ENUM ('walk','swim','fly','climb','burrow');
+-- EXCEPTION WHEN duplicate_object THEN NULL; END $$;
 
--- (Optional) Sizes
-DO $$ BEGIN
-  CREATE TYPE creature_size AS ENUM ('Tiny','Small','Medium','Large','Huge','Gargantuan');
-EXCEPTION WHEN duplicate_object THEN NULL; END $$;
+-- -- (Optional) Sizes
+-- DO $$ BEGIN
+--   CREATE TYPE creature_size AS ENUM ('Tiny','Small','Medium','Large','Huge','Gargantuan');
+-- EXCEPTION WHEN duplicate_object THEN NULL; END $$;
 
 -- (Optional) Alignments (only if you want to constrain them)
 DO $$ BEGIN
