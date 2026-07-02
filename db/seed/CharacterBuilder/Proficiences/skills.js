a7cce0f3dabd9f6312409f4eb9f0897959575220
Lyla Lynn <lyladlynn@Lylas-MacBook-Pro.local>
2026-07-02 10:24:42 -0500

    commented out code in skills.js because it isnt done and there was an error that bothered me.

--- db/seed/CharacterBuilder/Proficiences/skills.js
+++ db/seed/CharacterBuilder/Proficiences/skills.js
@@ -1,8 +1,8 @@
-import db from "../../../client";
+// import db from "../../../client";
 
-export async function seedSkills() {
-    try {
-        await db.query('TRUNCATE skills RESTART IDENTITY CASCADE');
-        const skillAbilityRel = (await db.query (`SELECT id FROM `))
-    }
-}
+// export async function seedSkills() {
+//     try {
+//         await db.query('TRUNCATE skills RESTART IDENTITY CASCADE');
+//         const skillAbilityRel = (await db.query (`SELECT id FROM `))
+//     }
+// }
